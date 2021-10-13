b0c62e53ff36b48633cea70031f2e6f524aaa6b3
Peter.Li <peter.li@pushpay.com>
2021-10-13 22:58:37 +1300

    refactor: 💡 currying item creator

--- src/items/index.ts
+++ src/items/index.ts
@@ -12,19 +12,21 @@ export enum ItemSKUOption {
   VGA = 'vga',
 }
 
+export const itemCreator =
+  (sku: string) => (name: string) => (price: number) => (): Item => {
+    return { sku, name, price }
+  }
+
+export const creatIpad = itemCreator(ItemSKUOption.IPD)('Super iPad')(549.99)
+export const creatMBP = itemCreator(ItemSKUOption.MBP)('MacBook Pro')(1399.99)
+export const creatATV = itemCreator(ItemSKUOption.ATV)('Apple TV')(109.5)
+export const creatVGA = itemCreator(ItemSKUOption.VGA)('VGA adapter')(30.0)
+
 const items = {
-  [ItemSKUOption.IPD]: (): Item => {
-    return { sku: ItemSKUOption.IPD, name: 'Super iPad', price: 549.99 }
-  },
-  [ItemSKUOption.MBP]: (): Item => {
-    return { sku: ItemSKUOption.MBP, name: 'MacBook Pro', price: 1399.99 }
-  },
-  [ItemSKUOption.ATV]: (): Item => {
-    return { sku: ItemSKUOption.ATV, name: 'Apple TV', price: 109.5 }
-  },
-  [ItemSKUOption.VGA]: (): Item => {
-    return { sku: ItemSKUOption.VGA, name: 'VGA adapter', price: 30.0 }
-  },
+  [ItemSKUOption.IPD]: (): Item => creatIpad(),
+  [ItemSKUOption.MBP]: (): Item => creatMBP(),
+  [ItemSKUOption.ATV]: (): Item => creatATV(),
+  [ItemSKUOption.VGA]: (): Item => creatVGA(),
 }
 
 export const createItem = (optin: ItemSKUOption): Item => {
