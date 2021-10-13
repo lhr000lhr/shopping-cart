1f6b2fb748d3e7f72453a9c6b9d271254d5cafab
Peter.Li <peter.li@pushpay.com>
2021-10-13 23:02:12 +1300

    test: 💍 add test for different creators

--- src/items/index.test.ts
+++ src/items/index.test.ts
@@ -1,32 +1,47 @@
-import { createItem, ItemSKUOption } from './index'
+import {
+  createItem,
+  ItemSKUOption,
+  creatATV,
+  creatIpad,
+  creatMBP,
+  creatVGA,
+} from './index'
 
 describe('Item creator', () => {
   test('should return Super iPad', () => {
-    expect(createItem(ItemSKUOption.IPD)).toEqual({
+    const item = {
       sku: 'ipd',
       name: 'Super iPad',
       price: 549.99,
-    })
+    }
+    expect(createItem(ItemSKUOption.IPD)).toEqual(item)
+    expect(creatIpad()).toEqual(item)
   })
   test('should return MacBook Pro', () => {
-    expect(createItem(ItemSKUOption.MBP)).toEqual({
+    const item = {
       sku: 'mbp',
       name: 'MacBook Pro',
       price: 1399.99,
-    })
+    }
+    expect(createItem(ItemSKUOption.MBP)).toEqual(item)
+    expect(creatMBP()).toEqual(item)
   })
   test('should return Apple tv', () => {
-    expect(createItem(ItemSKUOption.ATV)).toEqual({
+    const item = {
       sku: 'atv',
       name: 'Apple TV',
       price: 109.5,
-    })
+    }
+    expect(createItem(ItemSKUOption.ATV)).toEqual(item)
+    expect(creatATV()).toEqual(item)
   })
   test('should return VGA adapter', () => {
-    expect(createItem(ItemSKUOption.VGA)).toEqual({
+    const item = {
       sku: 'vga',
       name: 'VGA adapter',
-      price: 30.00,
-    })
+      price: 30.0,
+    }
+    expect(createItem(ItemSKUOption.VGA)).toEqual(item)
+    expect(creatVGA()).toEqual(item)
   })
 })
